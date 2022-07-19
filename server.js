6d26976a0537c9f33674990b44a240504028730f
Marcos Chong <mfchongm@hotmail.com>
2022-07-19 17:26:49 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const indexRouter = require("./routes/index.js");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(indexRouter);
+
+app.listen(process.env.PORT || 3000);
