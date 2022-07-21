6c3cda23cd92bdaba53f94ff746f94709ea325e4
Marcos Chong <mfchongm@hotmail.com>
2022-07-21 09:47:11 -0500

    Initial set up

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+//We have to check if we are in a dev or production environment
+//If we are not in production environment we can load our environment variables from env
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,6 +16,14 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
-app.use(indexRouter);
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
+
+app.use("/", indexRouter);
 
 app.listen(process.env.PORT || 3000);
