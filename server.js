8f8b72a4b1d6bb60bc9a0f761d3e4edbc11d08a2
Marcos Chong <mfchongm@hotmail.com>
2022-07-22 12:38:29 -0500

    Initial author routes

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index.js");
+const authorRouter = require("./routes/authors.js");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -16,6 +18,7 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -25,5 +28,6 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
